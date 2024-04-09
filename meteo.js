981f1731d931eb9a77b69271e56af4919da0ab98
jimni6 <nijimmy@outlook.fr>
2024-04-09 13:47:51 +0200

    Refactoring code using async/await instead of the promise chaining

--- meteo.js
+++ meteo.js
@@ -1,29 +1,30 @@
 
 // Charger la configuration de la ville depuis conf.json
-function weatherApp() {
-    fetch('conf.json')
-    .then(response => response.json())
-    .then(config => {  
+async function weatherApp() {
+    try {
+        const response = await fetch('conf.json');
+        const config = await response.json();
         // Appel de la fonction pour récupérer les données météo
-        getWeatherData(config);
-    })
-    .catch(error => console.error('Erreur lors du chargement de la configuration : ', error));
+        await getWeatherData(config);
+    } catch (error) {
+        console.error('Erreur lors du chargement de la configuration : ', error);
+    }
 }
 
-
 // Fonction pour récupérer les données météo depuis l'API
-function getWeatherData(config) {
-    fetch(`https://api.weatherapi.com/v1/forecast.json?key=${config.apiKey}&q=${config.city}&lang=en`)
-    .then(response => response.json())
-    .then(data => {
+async function getWeatherData(config) {
+    try {
+        const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=${config.apiKey}&q=${config.city}&lang=en`);
+        const data = await response.json();
         // Manipulation des données récupérées
-        displayWeatherData(data);
-    })
-    .catch(error => console.error('Erreur lors de la récupération des données météo : ', error));
+        await displayWeatherData(data);
+    } catch (error) {
+        console.error('Erreur lors de la récupération des données météo : ', error);
+    }
 }
 
 // Fonction pour afficher les données météo dans l'interface
-function displayWeatherData(data) {
+async function displayWeatherData(data) {
     const { text, icon } = data.current.condition;
     const { temp_c, feelslike_c, humidity } = data.current;
     const { name, country } = data.location;
@@ -38,7 +39,7 @@ function displayWeatherData(data) {
 
 weatherApp();
 
-// // Actualiser les données météo toutes les heures
+// // Décommenter la ligne ci-dessous pour actualiser toutes les heures
 // setInterval(testInterval, 3600000); // 3600000 ms = 1 heure
 
 module.exports = { displayWeatherData };
